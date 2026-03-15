40f71ae46395ffe40c3d2d84f8029790bd6aeb1a
Mandy Peng <mandypeng2007work@gmail.com>
2026-03-14 23:49:57 -0400

    removed test files

--- test.py
+++
@@ -1,8 +0,0 @@
-from rag import answer_question
-
-answer = answer_question(
-    "In what line is the lyric 'Now I'll just see you when I'm on business with everyone' on?",
-    pdf_path="sweetboy-lyrics.pdf"
-)
-
-print(answer)
